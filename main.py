71f65890b0ff5ed20af2ed3f3578327af164d51e
Izz Hafeez <izzhafeez@gmail.com>
2024-04-04 18:20:00 +0800

    Fix early return

--- main.py
+++ main.py
@@ -327,10 +327,7 @@ def quiz_score(quiz_type: str, quiz_name: str, request: QuizRequest, settings: A
         'plays': quiz['plays'] + 1
       }
     })
-    return {
-      'players': players,
-      'plays': quiz['plays'] + 1
-    }
+    return {}
 
   players = quiz['players']
   # save only the top 10 players
